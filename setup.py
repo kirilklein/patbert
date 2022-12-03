849eae69b9d17de450f3042d9d1360999baf0d6a
Kiril Klein <kikl@di.ku.dk>
2022-12-03 18:49:32 +0100

    changed names

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-setup(name='medbert',
+setup(name='patbert',
       version='1.0',
-      description='Pytorch Implementation of MedBERT',
+      description='Obtain patient embeddings form EHRs with BERT.',
       long_description=readme,
       author='Kiril Klein',
       author_email='kikl@di.ku.dk',
-      url="https://github.com/kirilklein/Med-BERT.git",
-      packages=['medbert'],
+      url="https://github.com/kirilklein/patbert.git",
+      packages=['patbert'],
      )
