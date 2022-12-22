1328ddcc19995445e11b0964afac586719caf3e4
Kiril Klein <kikl@di.ku.dk>
2022-12-22 16:33:05 +0100

    added embeddings for lab tests, test version

--- patbert/data/generate.py
+++ patbert/data/generate.py
@@ -7,7 +7,8 @@ import typer
 class DataGenerator(super):
     def __init__(self, num_patients, min_num_visits, max_num_visits, 
         min_num_codes_per_visit, max_num_codes_per_visit, min_los, 
-        max_los, num_codes):
+        max_los, num_codes, age_lower_bound=0, age_upper_bound=100,
+        num_different_lab_test=100, min_num_lab_tests=0, max_num_lab_tests=100):
         """
         Simulates data as lists:
             [pid, los_ls, all_visit_codes, visit_nums]
@@ -17,13 +18,20 @@ class DataGenerator(super):
         self.num_patients = num_patients
         self.min_num_codes_per_visit = min_num_codes_per_visit
         self.max_num_codes_per_visit = max_num_codes_per_visit
+        self.min_num_lab_tests = min_num_lab_tests
+        self.max_nun_lab_tests = max_num_lab_tests
         self.min_num_visits = min_num_visits
         self.max_num_visits = max_num_visits
         self.min_los = min_los
         self.max_los = max_los
         self.num_codes = num_codes
+        self.age_lower_bound = age_lower_bound
+        self.age_upper_bound = age_upper_bound
+        self.num_different_lab_test = num_different_lab_test
+        self.rng = np.radom.default_rng()
 
     def generate_ICD10_history(self, pid):
+        """Generates a list which contains the ICD10 codes (medbert format)"""
         codes = self.generate_randomICD10_codes(self.num_codes)
         num_visits = np.random.randint(self.min_num_visits, self.max_num_visits)
         num_codes_per_visit_ls = np.random.randint(self.min_num_codes_per_visit, 
@@ -36,6 +44,56 @@ class DataGenerator(super):
         visit_nums = np.arange(1, num_visits+1) # should start with 1!
         visit_nums = np.repeat(visit_nums, num_codes_per_visit_ls).tolist()
         return [pid, los_ls, all_visit_codes, visit_nums]
+    
+    # generate atc codes
+    def generate_patient_history(self, pid):
+        """Generates a dictionary which contains sex, ages, length of stay, codes, lab tests, lab tests visits"""
+        num_visits = self.rng.randint(self.min_num_visits, self.max_num_visits)
+        num_lab_tests = self.rng.randint(self.min_num_lab_tests, self.max_nun_lab_tests)
+        num_codes_per_visit_ls = np.random.randint(self.min_num_codes_per_visit, 
+            self.max_num_codes_per_visit, 
+            size=num_visits) # should icd and atc vectors point in different directions
+        ages = self.generate_ages(num_visits) 
+        
+        icd_codes = self.generate_randomICD10_codes(self.num_codes)
+        atc_codes = self.generate_randomATC_codes(self.num_codes)
+        codes = icd_codes + atc_codes
+        codes = np.random.choice(codes, 
+            size=np.sum(num_codes_per_visit_ls), replace=True).tolist()
+        lab_tests = self.generate_lab_tests(num_lab_tests)        
+        lab_tests_visits = self.generate_lab_tests_visits(num_lab_tests)
+        ages = self.generate_ages_in_days(self) # pass age as days or rounded years?
+        # simulate random increasing integers
+        # TODO: finish here
+        patient_dic = {
+            'pid':pid,
+            'sex':self.generate_sex(),
+            'ages':ages,
+            'los':los,
+            'codes_segments':codes_visits,
+            'codes':codes,
+            'lab_tests':lab_tests,
+            'lab_tests_segments':lab_tests_visits
+        }
+        return patient_dic
+
+    def generate_lab_tests(self, num_lab_tests):
+        lab_tests = []
+        for _ in range(num_lab_tests):
+            lab_tests.append(('test'+str(self.rng.randint(self.num_different_lab_test))), self.rng.normal())
+        return lab_tests
+    def generate_ages(self, num_visits):
+        ages = []
+        age_lower_bound = self.age_lower_bound
+        for i in range(num_visits):
+            random_age = self.rng.randint(age_lower_bound, self.age_upper_bound)
+            ages.append(random_age)
+            age_lower_bound = random_age + 1
+        
+        return ages
+
+    def generate_sex(self):
+        return self.rng.binomial(1, 0.5)
 
     def generate_randomICD10_codes(self, n):
         letters = np.random.choice([char for char in string.ascii_uppercase], 
@@ -43,6 +101,13 @@ class DataGenerator(super):
         numbers = np.random.choice(np.arange(1000), size=n, replace=True)
         codes = [letter + str(number).zfill(3)[:2] + '.' + str(number)[-1] for \
             letter, number in zip(letters, numbers)]
+        return codes
+    def generate_randomATC_codes(self, n):
+        letters = np.random.choice([char for char in string.ascii_uppercase], 
+            size=2*n, replace=True)
+        numbers = np.random.choice(np.arange(100), size=2*n, replace=True)
+        codes = [letter0 + str(number0).zfill(2) + letter1 + str(number1).zfill(2) for \
+            letter0, number0, letter1, number1 in zip(letters[:n], numbers[:n], letters[n:], numbers[n:])]
         return codes
 
     def simulate_data(self):
---
+++ patbert/tests/embeddings.py
@@ -0,0 +1,80 @@
+import torch
+from torch import nn
+# Define a vocabulary of 10 tokens
+vocab_size = 10
+
+# Define the dimensionality of the token embeddings
+embedding_dim = 2
+
+# Initialize the token embeddings randomly
+embeddings = torch.nn.Embedding(vocab_size, embedding_dim)
+
+# Define some example tokens as input
+tokens = torch.tensor([1, 2, 3, 4, 5, 1 ,2, 1, 2])
+# let's assume that token 2, 4, 5 are lab tests
+
+# Generate the token embeddings
+
+
+# Define a simple model that takes the token embeddings as input and 
+# predicts a target value
+class Model(nn.Module):
+    
+    def __init__(self):
+        # how many layers?
+        super().__init__()
+        self.embedding = torch.nn.Embedding(10, 20)
+        self.F1 = nn.Linear(20, 3)
+        self.F2 = nn.Linear(3, 1)
+    
+    def forward(self, x):
+        x = self.embedding(x)
+        x = self.F1(x)	
+        x = self.F2(x)
+        
+        return x
+model = Model()
+token_embeddings = model.embedding(tokens)
+print(token_embeddings.shape)
+multiplier = torch.ones(len(tokens))
+multiplier[2:6] = 1.4
+token_embeddings = token_embeddings * multiplier.reshape(-1, 1)
+# Print the generated token embeddings
+print(torch.mean(torch.linalg.vector_norm(token_embeddings, axis=1)))
+# Assuming that your embedding vectors are stored in a NumPy array called 'embeddings'
+norms = torch.linalg.norm(token_embeddings, axis=1, keepdims=True)
+# Define some target values for the model to predict
+targets = [1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.5   , 0.0, 0.0]
+# Define the loss function and optimizer
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+# Train the model
+"""
+for token, target in zip(tokens, targets):
+    # zero the parameter gradients
+    optimizer.zero_grad()
+    # Make predictions using the model
+    prediction = model(token)
+    # Calculate the loss
+    loss = loss_fn(prediction, torch.tensor(target))
+    
+    # Backpropagate the error and update the model parameters
+    loss.backward()
+    optimizer.step()
+"""
+targets = torch.tensor(targets)
+for i in range(10):
+    # zero the parameter gradients
+    optimizer.zero_grad()
+    # Make predictions using the model
+    predictions = model(tokens)
+
+    # Calculate the loss
+    loss = loss_fn(predictions.flatten(), targets)
+    
+    # Backpropagate the error and update the model parameters
+    loss.backward()
+    optimizer.step()    
+example_x = torch.ones((3, 2))
+#print(model.F1(example_x))
+#print(model.F1.weight)
