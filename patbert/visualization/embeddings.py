fa6cad390ad1adc9826dd34dd60e7202e1e7d9e0
Kiril Klein <kikl@di.ku.dk>
2022-12-30 14:34:25 +0100

    refactoring

--- patbert/visualization/embeddings.py
+++ patbert/visualization/embeddings.py
@@ -5,20 +5,13 @@ from patbert.common import vis
 plt.style.use('dark_background')
 
 class EmbeddingFigure():
-    def __init__(self, num_vectors, num_sub_vectors, num_sub_sub_vectors,  len, sub_len, sub_sub_len, 
-            num_sss_vecs=0, sss_len=0, s_vec=20, s_sub_vec=4, s_sub_sub_vec=.1, s_sss_vec=.01,
+    def __init__(self,vector_nums, vector_lens, marker_sizes,
             modalities = ['Disease', 'Medicine', 'Lab Test', 'Procedure', 'SEP', 'CLS', 'MASK'], seed=0,
             figsize=(10,10), dpi=100, fontsize=12, save_path=None, marker_multiplier=8, axins_kwargs=None,
             zoom_sub_vec_num = 9, text_x_shift=0, text_y_shift=0, add_inset=True):
         self.rng = np.random.default_rng(seed)
-        self.num_vectors = num_vectors 
-        self.num_sub_vectors = num_sub_vectors
-        self.num_sub_sub_vectors = num_sub_sub_vectors
-        self.num_sss_vecs = num_sss_vecs
-        self.len = len
-        self.sub_len = sub_len
-        self.sub_sub_len = sub_sub_len
-        self.sss_len = sss_len
+        self.vector_nums = vector_nums
+        self.vector_lens = vector_lens
         self.modalities = modalities
         self.text_x_shift = text_x_shift
         self.text_y_shift = text_y_shift
@@ -26,10 +19,9 @@ class EmbeddingFigure():
         self.dpi = dpi
         self.fontsize = fontsize
         self.save_path = save_path
-        self.s_vec = s_vec
-        self.s_sub_vec = s_sub_vec
-        self.s_sub_sub_vec = s_sub_sub_vec
-        self.s_sss_vec = s_sss_vec
+        self.marker_sizes = marker_sizes
+   
+   
         self.vecs = None
         self.sub_vecs = None
         self.sub_sub_vecs = None
@@ -46,16 +38,16 @@ class EmbeddingFigure():
         self.add_inset = add_inset
 
     def __call__(self):
-        assert len(self.modalities) == self.num_vectors, 'Number of modalities must match number of vectors'   
+        assert len(self.modalities) == self.vector_nums[0], 'Number of modalities must match number of vectors'   
         self.default_colors = self.default_colors*5
-        if self.num_sss_vecs > 0:
+        if self.vector_nums[3] > 0:
             self.vecs, self.sub_vecs, self.sub_sub_vecs, self.sss_vecs = self.get_vector_hierarchy()
         else:
             self.vecs, self.sub_vecs, self.sub_sub_vecs = self.get_vector_hierarchy()
         
         self.fig, self.ax = plt.subplots(figsize=self.figsize)
         self.scatter_vecs(self.ax)
-        for i in range(self.num_vectors):
+        for i in range(self.vector_nums[0]):
             self.ax.text(self.vecs[i,0]+self.text_x_shift, self.vecs[i,1]+self.text_y_shift, 
                 self.modalities[i], fontsize=self.fontsize, color='k',
                 bbox=dict(boxstyle="round",
@@ -84,37 +76,36 @@ class EmbeddingFigure():
         self.axins_kwargs['ylim'] = (sub_vec_y-y_width/2, sub_vec_y+y_width/2)
 
     def get_vector_colors(self):
-        self.sub_vector_color = np.repeat(self.default_colors, self.num_sub_vectors)
-        self.sub_sub_vector_colors = np.repeat(self.default_colors, self.num_sub_vectors*self.num_sub_sub_vectors)
-        if self.num_sss_vecs > 0:
-            self.sss_vector_colors = np.repeat(self.default_colors, self.num_sub_vectors*self.num_sub_sub_vectors*self.num_sss_vecs)
+        self.sub_vector_color = np.repeat(self.default_colors, self.vector_nums[1])
+        self.sub_sub_vector_colors = np.repeat(self.default_colors, self.vector_nums[1]*self.vector_nums[2])
+        if self.vector_nums[3] > 0:
+            self.sss_vector_colors = np.repeat(self.default_colors, self.vector_nums[1]*self.vector_nums[2]*self.vector_nums[3])
         
     def scatter_vecs(self, ax, marker_multiplier=1):
-        ax.scatter(self.vecs[:,0], self.vecs[:,1], s=self.s_vec*marker_multiplier, color=self.default_colors[:self.num_vectors])
-        ax.scatter(self.sub_vecs[:,0], self.sub_vecs[:,1], s=self.s_sub_vec*marker_multiplier, color=self.sub_vector_color[:self.num_sub_vectors*self.num_vectors])
-        ax.scatter(self.sub_sub_vecs[:,0], self.sub_sub_vecs[:,1], s=self.s_sub_sub_vec*marker_multiplier, 
-            color=self.sub_sub_vector_colors[:self.num_sub_sub_vectors*self.num_sub_vectors*self.num_vectors])
-        if self.num_sss_vecs > 0:
-            ax.scatter(self.sss_vecs[:,0], self.sss_vecs[:,1], s=self.s_sss_vec*marker_multiplier, 
-                color=self.sss_vector_colors[:self.num_sss_vecs*self.num_sub_sub_vectors*self.num_sub_vectors*self.num_vectors])
+        ax.scatter(self.vecs[:,0], self.vecs[:,1], s=self.marker_sizes[0]*marker_multiplier, color=self.default_colors[:self.vector_nums[0]])
+        ax.scatter(self.sub_vecs[:,0], self.sub_vecs[:,1], s=self.marker_sizes[1]*marker_multiplier, color=self.sub_vector_color[:self.vector_nums[1]*self.vector_nums[0]])
+        ax.scatter(self.sub_sub_vecs[:,0], self.sub_sub_vecs[:,1], s=self.marker_sizes[2]*marker_multiplier, 
+            color=self.sub_sub_vector_colors[:self.vector_nums[2]*self.vector_nums[1]*self.vector_nums[0]])
+        if self.vector_nums[3] > 0:
+            ax.scatter(self.sss_vecs[:,0], self.sss_vecs[:,1], s=self.marker_sizes[3]*marker_multiplier, 
+                color=self.sss_vector_colors[:self.vector_nums[3]*self.vector_nums[2]*self.vector_nums[1]*self.vector_nums[0]])
 
     def get_vector_hierarchy(self):
-        all_vecs = []
-        vectors = self.get_vec(self.num_vectors,self.len)
+        vectors = self.get_vec(self.vector_nums[0],self.vector_lens[0])
         sub_vec = []
         sub_sub_vec = []
         sss_vec = []
-        for i in range(self.num_vectors):
-            sub_vec.append(vectors[i] + self.get_vec(self.num_sub_vectors, self.sub_len))
-            for j in range(self.num_sub_vectors):
-                sub_sub_vec.append(sub_vec[i][j] + self.get_vec(self.num_sub_sub_vectors,self.sub_sub_len))
+        for i in range(self.vector_nums[0]):
+            sub_vec.append(vectors[i] + self.get_vec(self.vector_nums[1], self.vector_lens[1]))
+            for j in range(self.vector_nums[1]):
+                sub_sub_vec.append(sub_vec[i][j] + self.get_vec(self.vector_nums[2],self.vector_lens[2]))
        
         sub_vec = np.concatenate(sub_vec, axis=0)
         sub_sub_vec = np.concatenate(sub_sub_vec, axis=0)
-        if self.num_sss_vecs > 0:
+        if self.vector_nums[3] > 0:
             for k in range(len(sub_sub_vec)):
-                sss_vec.append(sub_sub_vec[k] + self.get_vec(self.num_sss_vecs, self.sss_len))
-        if self.num_sss_vecs > 0:
+                sss_vec.append(sub_sub_vec[k] + self.get_vec(self.vector_nums[3], self.vector_lens[3]))
+        if self.vector_nums[3] > 0:
             sss_vec = np.concatenate(sss_vec, axis=0)
             return vectors, sub_vec, sub_sub_vec, sss_vec
         else:
