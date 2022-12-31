29644b7cc9e2e27ae20d215f4f179898be39685a
Kiril Klein <kikl@di.ku.dk>
2022-12-31 17:23:17 +0100

    debugged, refactored

--- patbert/visualization/embeddings.py
+++ patbert/visualization/embeddings.py
@@ -13,7 +13,7 @@ class EmbeddingFigure():
         plt.rcParams.update(plt.rcParamsDefault)
         plt.style.use(style)
         self.rng = np.random.default_rng(seed)
-        self.vector_nums = vector_nums
+        self.vec_nums = vector_nums
         self.vector_lens = vector_lens
         self.modalities = modalities
         self.text_x_shift = text_x_shift
@@ -26,21 +26,21 @@ class EmbeddingFigure():
    
         self.vecs = [] # list of arrays of vectors
         self.axins_kwargs = axins_kwargs
-        self.default_colors = self.get_nice_colors_dark_background()
-        self.get_vector_colors()
+        self.def_colors = self.get_nice_colors_dark_background()
+        self.get_vec_colors()
         self.marker_multiplier = marker_multiplier
         self.axins = None
         self.zoom_sub_vec_num = zoom_sub_vec_num
         self.add_inset = add_inset
 
     def __call__(self):
-        assert len(self.modalities) == self.vector_nums[0], 'Number of modalities must match number of vectors'   
-        self.default_colors = self.default_colors*5
+        assert len(self.modalities) == self.vec_nums[0], 'Number of modalities must match number of vectors'   
+        self.def_colors = self.def_colors*5
         self.vecs = self.get_vector_hierarchy()
             
         self.fig, self.ax = plt.subplots(figsize=self.figsize)
         self.scatter_vecs_for(self.ax)
-        for i in range(self.vector_nums[0]):
+        for i in range(self.vec_nums[0]):
             self.ax.text(self.vecs[0][i,0]+self.text_x_shift, self.vecs[0][i,1]+self.text_y_shift, 
                 self.modalities[i], fontsize=self.fontsize, color='k',
                 bbox=dict(boxstyle="round",
@@ -67,30 +67,30 @@ class EmbeddingFigure():
         y_width = abs(self.axins_kwargs['ylim'][1] - self.axins_kwargs['ylim'][0])
         self.axins_kwargs['ylim'] = (sub_vec_y-y_width/2, sub_vec_y+y_width/2)
 
-    def get_vector_colors(self):
+    def get_vec_colors(self):
         """Get colors for each vector"""
-        sub_vector_colors = np.repeat(self.default_colors, self.vector_nums[1]) # repeat colors for each sub vector
-        sub_vector_colors = sub_vector_colors[:self.vector_nums[1]*self.vector_nums[0]] # trim to number of vectors
-        self.vector_colors = [sub_vector_colors]
+        sub_vec_colors = np.repeat(self.def_colors, self.vec_nums[1]) # repeat colors for each sub vector
+        sub_vec_colors = sub_vec_colors[:self.vec_nums[1]*self.vec_nums[0]] # trim to number of vectors
+        self.vec_colors = [sub_vec_colors]
 
-        sub_sub_vector_colors = np.repeat(self.default_colors, self.vector_nums[1]*self.vector_nums[2]) # repeat colors for each sub vector
-        sub_sub_vector_colors = sub_sub_vector_colors[:self.vector_nums[2]*self.vector_nums[1]*self.vector_nums[0]] # trim to number of vectors
-        self.vector_colors.append(sub_sub_vector_colors)
+        sub_sub_vec_colors = np.repeat(self.def_colors, self.vec_nums[1]*self.vec_nums[2]) # repeat colors for each sub vector
+        sub_sub_vec_colors = sub_sub_vec_colors[:self.vec_nums[2]*self.vec_nums[1]*self.vec_nums[0]] # trim to number of vectors
+        self.vec_colors.append(sub_sub_vec_colors)
 
-        if self.vector_nums[3] > 0:
-            sss_vector_colors = np.repeat(self.default_colors, self.vector_nums[1]*self.vector_nums[2]*self.vector_nums[3])
-            sss_vector_colors = self.sss_vector_colors[:self.vector_nums[3]*self.vector_nums[2]*self.vector_nums[1]*self.vector_nums[0]] # trim to number of vectors
-            self.vector_colors.append(sss_vector_colors)
+        if self.vec_nums[3] > 0:
+            sss_vec_colors = np.repeat(self.def_colors, self.vec_nums[1]*self.vec_nums[2]*self.vec_nums[3])
+            sss_vec_colors = sss_vec_colors[:self.vec_nums[3]*self.vec_nums[2]*self.vec_nums[1]*self.vec_nums[0]] # trim to number of vectors
+            self.vec_colors.append(sss_vec_colors)
 
     def scatter_vecs_for(self, ax, marker_multiplier=1):
         """Produce scatter plot"""
         for i, modality in enumerate(self.modalities):
             ax.scatter(self.vecs[0][i,0], self.vecs[0][i,1], s=self.marker_sizes[0]*marker_multiplier, 
-                color=self.default_colors[i])
+                color=self.def_colors[i])
             if modality in ['SEP', 'CLS','MASK']:
                 continue # skip sub vectors for these modalities
-            id0, id1 = i*self.vector_nums[1], (i+1)*self.vector_nums[1]
-            colors = self.vector_colors[0]
+            id0, id1 = i*self.vec_nums[1], (i+1)*self.vec_nums[1]
+            colors = self.vec_colors[0]
             if modality == 'Lab Test':
                 # multiply vectors by random number
                 multiplier = self.rng.uniform(0.5, .9, size=id1-id0)
@@ -106,34 +106,34 @@ class EmbeddingFigure():
                 ax.scatter(self.vecs[1][id0:id1,0], self.vecs[1][id0:id1,1], 
                         s=self.marker_sizes[1]*marker_multiplier, color=colors[id0:id1])
             
-            id0, id1 = i*self.vector_nums[1]*self.vector_nums[2], (i+1)*self.vector_nums[1]*self.vector_nums[2]
-            colors = self.vector_colors[1]
+            id0, id1 = i*self.vec_nums[1]*self.vec_nums[2], (i+1)*self.vec_nums[1]*self.vec_nums[2]
+            colors = self.vec_colors[1]
             ax.scatter(self.vecs[2][id0:id1,0], self.vecs[2][id0:id1,1], s=self.marker_sizes[2]*marker_multiplier, 
                 color=colors[id0:id1])
-            if self.vector_nums[3] > 0:
-                id0, id1 = i*self.vector_nums[1]*self.vector_nums[2]*self.vector_nums[3], (i+1)*self.vector_nums[1]*self.vector_nums[2]*self.vector_nums[3]
-                colors = self.vector_colors[2]
+            if self.vec_nums[3] > 0:
+                id0, id1 = i*self.vec_nums[1]*self.vec_nums[2]*self.vec_nums[3], (i+1)*self.vec_nums[1]*self.vec_nums[2]*self.vec_nums[3]
+                colors = self.vec_colors[2]
                 ax.scatter(self.vecs[3][id0:id1,0], self.vecs[3][id0:id1,1], s=self.marker_sizes[3]*marker_multiplier, 
                     color=colors[id0:id1])
 
     def get_vector_hierarchy(self):
         """Create cluster of vectors for each modality, then create sub vectors for each cluster, then sub-sub vectors, etc.
         Returns list where each entry is an array of vectors for that level of hierarchy."""
-        vectors = self.get_vec(self.vector_nums[0],self.vector_lens[0])
+        vectors = self.get_vec(self.vec_nums[0],self.vector_lens[0])
         sub_vec = []
         sub_sub_vec = []
         sss_vec = []
-        for i in range(self.vector_nums[0]):
-            sub_vec.append(vectors[i] + self.get_vec(self.vector_nums[1], self.vector_lens[1]))
-            for j in range(self.vector_nums[1]):
-                sub_sub_vec.append(sub_vec[i][j] + self.get_vec(self.vector_nums[2],self.vector_lens[2]))
+        for i in range(self.vec_nums[0]):
+            sub_vec.append(vectors[i] + self.get_vec(self.vec_nums[1], self.vector_lens[1]))
+            for j in range(self.vec_nums[1]):
+                sub_sub_vec.append(sub_vec[i][j] + self.get_vec(self.vec_nums[2],self.vector_lens[2]))
        
         sub_vec = np.concatenate(sub_vec, axis=0)
         sub_sub_vec = np.concatenate(sub_sub_vec, axis=0)
-        if self.vector_nums[3] > 0:
+        if self.vec_nums[3] > 0:
             for k in range(len(sub_sub_vec)):
-                sss_vec.append(sub_sub_vec[k] + self.get_vec(self.vector_nums[3], self.vector_lens[3]))
-        if self.vector_nums[3] > 0:
+                sss_vec.append(sub_sub_vec[k] + self.get_vec(self.vec_nums[3], self.vector_lens[3]))
+        if self.vec_nums[3] > 0:
             sss_vec = np.concatenate(sss_vec, axis=0)
             return [vectors, sub_vec, sub_sub_vec, sss_vec]
         else:
