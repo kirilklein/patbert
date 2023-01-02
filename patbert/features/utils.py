e4f7a5f920e484222012184bf2b3389e44b69e3a
Kiril Klein <kikl@di.ku.dk>
2023-01-02 11:34:57 +0100

    fixed bug

--- patbert/features/utils.py
+++ patbert/features/utils.py
@@ -3,23 +3,31 @@ import numpy as np
 from numpy.random import default_rng
 rng = default_rng()
 
-#TODO dont mask cls and sep tokens
-def random_mask(codes, vocab, mask_prob=0.15):
+
+def random_mask(idxs, vocab, mask_prob=0.15,
+    special_tokens=['<CLS>', '<PAD>', '<SEP>', '<MASK>', '<UNK>', ], seed=0):
     """mask code with 15% probability, 80% of the time replace with [MASK], 
         10% of the time replace with random token, 10% of the time keep original"""
-    masked_codes = codes
-    labels = len(codes) * [-100] # -100 is ignored by loss function
-    prob = rng.uniform()
-    for i, _ in enumerate(masked_codes):
+    rng = default_rng(seed)
+    masked_idxs = idxs.copy()
+    special_idxs = [vocab[token] for token in special_tokens]
+    labels = len(idxs) * [-100] # -100 is ignored by loss function
+    
+    for i, idx in enumerate(idxs):
+        if idx in special_idxs:
+            continue
+        prob = rng.uniform()
         if prob<mask_prob:
-            prob = rng.uniform()   
+            prob = rng.uniform()  
+            # 80% of the time replace with [MASK] 
             if prob < 0.8:
-                masked_codes[i] = vocab['<MASK>']
-                labels[i] = vocab['<UNK>']
-            # 10% randomly change token to random token
+                masked_idxs[i] = vocab['<MASK>']
+            # 10% change token to random token
             elif prob < 0.9:
-                masked_codes[i] = rng.choice(list(vocab.values())[9:]) # first 9 tokens are special!
-    return masked_codes, labels
+                masked_idxs[i] = rng.choice(list(vocab.values())[len(special_idxs):]) # first tokens are special!
+            # 10% keep original
+            labels[i] = idx
+    return masked_idxs, labels
 
 def seq_padding(seq, max_len, vocab):
     return seq + (max_len-len(seq)) * [vocab['<PAD>']]
