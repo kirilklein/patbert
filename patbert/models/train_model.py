d8e28a70dabf940bf9bf781089c58a520d222ff5
Kiril Klein <kikl@di.ku.dk>
2023-01-26 10:33:09 +0100

    improved configs

--- patbert/models/train_model.py
+++ patbert/models/train_model.py
@@ -2,17 +2,21 @@ from os.path import dirname, join, realpath
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
-from patbert.models import utils
+
 from patbert.common import common
+from patbert.models import utils
 
-from torch import optim
-
-config_name = "test"
+config_name = "config"
 base_dir = dirname(dirname(dirname(realpath(__file__))))
 config_path = join(base_dir, 'configs')
 
 @hydra.main(version_base=None, config_path=config_path, config_name=config_name)
 def my_app(cfg: DictConfig) -> None:
+    print(cfg.data.channels)
+    print(cfg.data.mask_prob)
+    print(cfg.data.dir)
+    print(cfg.model.name)
+    print(cfg.model.hidden_size)
     data = common.load_data(cfg)
     model = utils.get_model(data, cfg)
     trainer = utils.CustomPreTrainer(data, model, cfg)
