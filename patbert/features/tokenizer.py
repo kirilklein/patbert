4faaca5754ac22ad4deb6f31c8d10a05678e7b97
Kiril Klein <kikl@di.ku.dk>
2023-03-19 16:44:32 +0100

    renaming to synchronize with ehr2vec

--- patbert/features/tokenizer.py
+++ patbert/features/tokenizer.py
@@ -1,14 +1,8 @@
-import pickle as pkl
 from collections import defaultdict
-from os.path import dirname, join, realpath
 
 import torch
-import typer
 from tqdm import tqdm
 
-from patbert.features import utils
-
-from patbert.common import medical
 
 class BaseTokenizer():
     def __init__(self, pad_len, vocabulary=None):
@@ -27,9 +21,9 @@ class BaseTokenizer():
 
     def batch_encode(self, seqs):
         seqs['idx'] = []
-        for i in tqdm(range(len(seqs['concept']))):
-            seqs['idx'].append(self.encode_seq(seqs['concept'][i]))
-        del seqs['concept']
+        for i in tqdm(range(len(seqs['concepts']))):
+            seqs['idx'].append(self.encode_seq(seqs['concepts'][i]))
+        del seqs['concepts']
         return seqs
 
     def encode_seq(self, seq):
@@ -45,11 +39,10 @@ class BaseTokenizer():
         return self.vocabulary[code]
 
 
-class EHRTokenizer(BaseTokenizer):
+class EHRTokenizer():
     """EHR tokenizer with background sentent and different channels"""
 
-    def __init__(self, pad_len, cfg, vocabulary=None, len_background=5):
-        super(self, EHRTokenizer).__init__(pad_len, cfg)
+    def __init__(self, vocabulary=None):
         """Add description"""
         if isinstance(vocabulary, type(None)):
             self.special_tokens = [
@@ -63,68 +56,49 @@ class EHRTokenizer(BaseTokenizer):
             self.vocabulary = {token: idx for idx,
                                token in enumerate(self.special_tokens)}
             for i in range(1900, 2022):
-                self.vocabulary[f'[BIRTHYEAR]{i}'] = len(self.vocabulary)
+                self.vocabulary[f'[BIRTHYEAR_{i}]'] = len(self.vocabulary)
             for i in range(1, 13):
-                self.vocabulary[f'[BIRTHMONTH]{i}'] = len(self.vocabulary)
+                self.vocabulary[f'[BIRTHMONTH_{i}]'] = len(self.vocabulary)
         else:
             self.vocabulary = vocabulary
-        self.len_background = len_background  # usually cls, sex, birthyear, birthmonth
 
-    def encode_seq(self, seq):
+    def __call__(self, seq):        
         self.enc_seq = defaultdict(list)  # we need a dictionary of lists
         self.seq = seq
         first_visit = 1
-        abs_pos_0 = seq['abs_pos'][0]
+        abs_pos_0 = seq['abspos'][0]
         last_abs_pos = 0
 
         # skip pid, birthdate and sex
-        for i in range(len(seq['concept'])):
+        for i in range(len(seq['concepts'])):
             if seq['segment'][i] > first_visit:
                 self.append_code_idx('[SEP]')
                 self.enc_seq['segment'].append(first_visit)
                 self.append_previous_token('ages')
-                self.append_previous_token('abs_pos')
-                self.enc_seq['los'].append(
-                    seq['abs_pos'][i - 1] - abs_pos_0)  # visit length in days
-                last_abs_pos = seq['abs_pos'][i]
+                self.append_previous_token('abspos')
+                last_abs_pos = seq['abspos'][i]
                 self.enc_seq['values'].append(1)
                 first_visit = seq['segment'][i]
-                if len(seq['abs_pos']) > (i + 1):
-                    abs_pos_0 = seq['abs_pos'][i + 1]  # abs pos of next visit
-            for key in ['segment', 'concept', 'ages', 'abs_pos', 'values']:
+                if len(seq['abspos']) > (i + 1):
+                    abs_pos_0 = seq['abspos'][i + 1]  # abs pos of next visit
+            for key in ['segment', 'concepts', 'ages', 'abspos', 'values']:
                 self.append_token_from_original(key, i)
-            self.enc_seq['los'].append(0)
-            self.enc_seq['idx'].append(self.encode(seq['concept'][i]))
-            if i == (len(seq['concept']) - 1):
-                self.enc_seq['los'].append(seq['abs_pos'][i] - last_abs_pos)
+            self.enc_seq['idx'].append(self.encode(seq['concepts'][i]))
+            if i == (len(seq['concepts']) - 1):
+                self.enc_seq['los'].append(seq['abspos'][i] - last_abs_pos)
         self.append_last_sep_token()
         self.truncate()
         self.insert_first_sep_token()
-        self.construct_background_sentence()
         return dict(self.enc_seq)
 
     def append_code_idx(self, code):
         """We append code to codes and the corresponding index to idx."""
-        self.enc_seq['concept'].append(code)
+        self.enc_seq['concepts'].append(code)
         self.enc_seq['idx'].append(self.encode(code))
-
-    def construct_background_sentence(self):
-        birthdate = self.seq['birthdate']
-        sex = self.seq['sex']
-        background_codes = [f'[BIRTHMONTH>{birthdate.month}',
-                            f'[BIRTHYEAR>{birthdate.year}', f'[SEX>{sex}', '[CLS]']  # order important
-        for code in background_codes:
-            self.insert_code_idx(code)
-
-        for key in ['segment', 'ages',
-                    'abs_pos', 'los']:  # fill other lists with zeros
-            self.insert_values(len(background_codes), key, 0)
-        self.insert_values(len(background_codes), 'values',
-                           1)  # fill values with 1
 
     def insert_code_idx(self, code):
         """We insert code to codes and the corresponding index to idx."""
-        self.enc_seq['concept'].insert(0, code)
+        self.enc_seq['concepts'].insert(0, code)
         self.enc_seq['idx'].insert(0, self.encode(code))
 
     def insert_values(self, n, key, value=0):
@@ -162,10 +136,10 @@ class EHRTokenizer(BaseTokenizer):
         if not isinstance(self.max_len, type(None)):
             max_len = self.max_len - self.len_background  # background sentence + CLS token
             if len(self.enc_seq['idx']) > max_len:
-                if self.enc_seq['concept'][-max_len] == '[SEP]':
+                if self.enc_seq['concepts'][-max_len] == '[SEP]':
                     # we don't want to start a sequence with SEP
                     max_len = max_len - 1
-                for key in ['concept', 'idx', 'values',
+                for key in ['concepts', 'idx', 'values',
                             'segment', 'ages', 'abs_pos', 'los']:
                     self.truncate_ls(key, max_len)
 
@@ -181,7 +155,4 @@ class EHRTokenizer(BaseTokenizer):
         print(f"Writing vocab to {dest}")
         torch.save(self.vocabulary, dest)
 
-class HierarchicalTokenizer():
-    """Tokenizer for hierarchical data"""
-    def __init__(self, max_len=None, len_background=0,):
 
